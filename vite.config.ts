a0788d01093cdff7853c8214897dc580b0b331b6
Sithembiso Chivambo <schivambo@uj.ac.za>
2026-03-24 23:11:32 +0200

    capacity

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: mode === "development" ? "/" : "/villagekota/",
+  //base: mode === "development" ? "/" : "/villagekota/",
+  base: "./",
   server: {
     host: "::",
     port: 8080,
